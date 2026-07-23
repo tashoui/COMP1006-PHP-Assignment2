894f7f2f1e75e67b668bf6ba12fed59c26d4454b
Natasha Bertrand <Tashagirl@hotmail.ca>
2026-07-23 15:32:13 -0400

    finished configuration file and added index file

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+ // Define database host
+  define("DB_HOST", "172.31.22.43");
+  // Define the name of the database that we will be using
+  define("DB_NAME", "Natasha200458345"); // replace with your info
+  // Define the username for your Database name
+  define("DB_USER", "Natasha200458345"); // replace with your info
+  // Define the password for the user
+  define("DB_PASS", "EEPKL1owS2"); // replace with your info
+?>
