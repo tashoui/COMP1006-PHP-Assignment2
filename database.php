54dd7978f8a47930e171b2f7e7a7fb0182db684a
Natasha Bertrand <Tashagirl@hotmail.ca>
2026-07-23 15:16:01 -0400

    Created orders table in database and setup database connection

---
+++ database.php
@@ -0,0 +1,36 @@
+class Database{
+    private $host;
+    private $dbName;
+    private $username;
+    private $password;
+
+    private $pdoInstance = null;
+
+    public function __construct($host, $dbName, $username, $password){
+        $this->host = 	$host;
+        $this->dbName = $dbName;
+        $this->username = $username;
+        $this->password = $password;
+    }
+
+    public function connect(){
+
+        if($this->pdoInstance !==null){
+            return $this->pdoInstance;
+        }
+
+        $dsn = "mysql:host={$this->host};dbname={$this->dbName};charset=utf8mb4";
+
+        $options = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+            PDO::ATTR_EMULATE_PREPARES => false,
+        ];
+
+        try{
+            $this->pdoInstance = new PDO($dsn, $this->username, $this->password, $options);
+            return $this->pdoInstance;}catch(PDOException $e){
+            die("Database connection failed: " . $e->getMessage());
+        }
+    }
+}
